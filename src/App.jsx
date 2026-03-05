94f5ae1aab37110d71ea98db2195dafc6decf4f0
Debabrata Das <debabratacmt@gmail.com>
2026-03-06 00:51:24 +0530

    add toast

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navber from './Component/Navbar/Navber'
 import TicketList from './Component/TicketList/TicketList'
 import TaskStatus from './TaskStatus/TaskStatus'
 import useTicket from './useTicket/useTicket'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const { tickets, tasks, resolved, addTask, completeTask } = useTicket();
@@ -14,20 +16,20 @@ function App() {
     <>
       <Navber></Navber>
       <Banner
-      inProgress ={tasks.length}
-      resolved = {resolved.length}
+        inProgress={tasks.length}
+        resolved={resolved.length}
       ></Banner>
 
       <div className='max-w-[1200px] mx-auto p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
         <div className="lg:col-span-2">
           <TicketList tickets={tickets}
-           addTask={addTask}
+            addTask={addTask}
           />
         </div>
         <TaskStatus
-        tasks={tasks}
-        resolved={resolved}
-        completeTask={completeTask}
+          tasks={tasks}
+          resolved={resolved}
+          completeTask={completeTask}
         ></TaskStatus>
       </div>
 
@@ -35,6 +37,7 @@ function App() {
 
 
       <Footer></Footer>
+      <ToastContainer position="top-right" autoClose={2000}></ToastContainer>
     </>
   )
 }
--- src/useTicket/useTicket.js
+++ src/useTicket/useTicket.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ticketData from "../../public/data.json"
+import { toast } from 'react-toastify';
 
 const useTicket = () => {
     const [tickets, setTickets] = useState(ticketData);
@@ -9,11 +10,13 @@ const useTicket = () => {
     const addTask = (ticket) => {
         setTasks([...tasks, ticket]);
         setTickets(tickets.filter((t) => t.id !== ticket.id));
+        toast.success("Ticket added to In Progress!");
     };
 
     const completeTask = (ticket) => {
         setTasks(tasks.filter((t) => t.id !== ticket.id));
         setResolved([...resolved, ticket]);
+        toast.info("Task marked as completed!");
     };
 
     return {
